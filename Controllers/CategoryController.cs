87487dbf274d15b8c62a7656475e6edde8ffcf17
Asmaa Rahy <111569346+AsmaaRahy@users.noreply.github.com>
2024-04-17 00:29:25 +0200

    Add files via upload

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using ETickets.IRepository;
+using ETickets.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETickets.Controllers
+{
+    public class CategoryController : Controller
+    {
+        ICategoryRepository categoryRepository;
+
+        public CategoryController(ICategoryRepository categoryRepository) {
+            this.categoryRepository = categoryRepository;
+        }
+        public IActionResult Index()
+        {
+            var listOfCategories = categoryRepository.ReadAll();
+            return View("Index" ,listOfCategories);
+        }
+
+        public IActionResult ShowAll(int id)
+        {
+            var movies = categoryRepository.ReadAllMovies(id);
+            
+            //var moviesInCat = categoryRepository.ReadAllMovies(id);
+            return View("ShowAll", movies);
+        }
+
+    }
+}
